5989cd82962f1998c341f37a032e2b80b40f09b8
Ivomatter <ivogelezarov@gmail.com>
2023-04-15 16:10:16 +0300

    Initial commit

---
+++ Library/Book.h
@@ -0,0 +1,23 @@
+#pragma once
+
+
+
+#include <string>
+#include <vector>
+
+
+class Book
+{
+private:
+	std::string Author;
+	std::string Title;
+	std::string Genre;
+	std::string Description;
+	unsigned year;
+	std::vector<std::string> tags;
+	double rating;
+	unsigned id;
+public:
+
+};
+
